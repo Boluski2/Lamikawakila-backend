1ac51e14024f5bffcd047b4c9eb85032e0580186
Boluski1 <babatundebolu@rocketmail.com>
2026-03-31 10:00:15 +0100

    serverless

--- index.js
+++ index.js
@@ -293,10 +293,10 @@
 
 
 
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import dotenv from 'dotenv';
+// import express from 'express';
+// import bodyParser from 'body-parser';
+// import cors from 'cors';
+// import dotenv from 'dotenv';
 import { Resend } from 'resend';
 
 dotenv.config();
